1a09f328a9a8e63312a8c3a25d50215c48632667
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-07-23 20:39:30 +0200

    BUG correction

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     data = serial.readLine()
+    transfert = 0
     datalogger.log(
     datalogger.createCV("type", "respons"),
     datalogger.createCV("message", data)
@@ -28,6 +29,7 @@ bluetooth.onBluetoothDisconnected(function () {
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     serial.writeLine(command)
+    transfert = 1
     datalogger.log(
     datalogger.createCV("type", "command"),
     datalogger.createCV("message", command)
@@ -39,6 +41,7 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
     basic.clearScreen()
 })
 let command = ""
+let transfert = 0
 let data_tot = ""
 let data = ""
 basic.showIcon(IconNames.House)
@@ -48,5 +51,10 @@ datalogger.setColumnTitles("type")
 datalogger.setColumnTitles("message")
 serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-	
+    if (transfert == 1) {
+        transfert += 1
+        if (transfert > 3) {
+            basic.showIcon(IconNames.Sad)
+        }
+    }
 })
