6e29e4dc58bc5fb8d8bf1bf4c0a8672c06727ed5
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-07-23 18:34:01 +0200

    fonction avec probleme de charactere au retour

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    datalogger.log(
+    datalogger.createCV("type", "respons"),
+    datalogger.createCV("message", serial.readLine())
+    )
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
     bluetooth.uartWriteString(serial.readLine())
+    basic.pause(1000)
+    basic.clearScreen()
 })
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.StickFigure)
@@ -9,11 +22,21 @@ bluetooth.onBluetoothDisconnected(function () {
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     serial.writeLine(bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
+    datalogger.log(
+    datalogger.createCV("type", "command"),
+    datalogger.createCV("message", bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
+    )
+    basic.showIcon(IconNames.Sword)
     if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)) == "close") {
         basic.showIcon(IconNames.No)
     }
+    basic.pause(1000)
+    basic.clearScreen()
 })
 basic.showIcon(IconNames.House)
+datalogger.setColumnTitles("type")
+datalogger.setColumnTitles("message")
+serial.setBaudRate(BaudRate.BaudRate9600)
 basic.forever(function () {
 	
 })
